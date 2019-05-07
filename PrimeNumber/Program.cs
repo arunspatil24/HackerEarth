fdf23a34471fcfc698a91b30806a9df7b0d5cdd7
arunspatil24 <33492857+arunspatil24@users.noreply.github.com>
2019-05-08 00:57:28 +0530

    4th program

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 2; i < n; i++)
+        {
+            int count = 0;
+            for (int j = 1; j < i; j++)
+            {
+                if (i % j == 0)
+                    count++;
+            }
+            if (count < 2)
+                Console.Write(i + " ");
+        }
+
+    }
+}
