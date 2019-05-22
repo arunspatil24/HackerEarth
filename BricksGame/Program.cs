07c1992a7fe71a10151d397e016e3fea9f5402b0
arunspatil24 <33492857+arunspatil24@users.noreply.github.com>
2019-05-22 11:05:55 +0530

    time Problem

---
+++ BricksGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        string s2 = s.Substring(0, 2);
+        int ch = 0;
+        if (s[8] == 'P')
+        {
+            ch = int.Parse(s2) + 12;
+            s2 = ch.ToString();
+        }
+        if (s[8] == 'A')
+        {
+            if (int.Parse(s2) == 12)
+            {
+                s2 = "00";
+            }
+        }
+        
+        for(int i = 2; i < s.Length - 2; i++)
+        {
+            s2 = s2 + s[i];
+        }
+        Console.WriteLine(s2);
+    }
+}
